7c40e8afd76203973eaff7945960e4d22d3951e0
Pranav Vichare <pranavvichare361@gmail.com>
2024-06-19 01:14:53 -0500

    Update Code

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 const http = require("http");
+const fs = require("fs");
+const url = require("url");
 
-const myServer = http.createServer();
+const myServer = http.createServer((req, res) => {
+  if (req.url === "/favicon.ico") return res.end();
+  const log = `${Date.now()}: ${req.url} New Request Received\n`;
+  const parsedUrl = url.parse(req.url, true);
+  console.log(parsedUrl);
+  fs.appendFile("log.txt", log, (err, data) => {
+    switch (parsedUrl.pathname) {
+      case "/":
+        res.end("Welcome to my Server!");
+        break;
+      case "/about":
+        const username = parsedUrl.query.id;
+        res.end(`Hello, ${username}! This is the About Page.`);
+        break;
+      default:
+        res.end("Page not found!");
+        break;
+    }
+  });
+});
+
+myServer.listen(8000, () => console.log("Server started!!"));
