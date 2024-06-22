1cba35520220b784a3c127ecfe7f5ae9ff4a1e12
Pranavv361 <pranavvichare361@gmail.com>
2024-06-22 12:00:56 -0500

    Change the code.

--- index.js
+++ index.js
@@ -6,7 +6,28 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = 8000;
 
-// Middleware
+//Connection to MongoDB
+mongoose
+  .connect("mongodb://127.0.0.1:27017/Nodejs-app")
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log("MongoDB connection failed:", err));
+
+// Schema
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: false },
+    email: { type: String, required: true, unique: true },
+    job_title: { type: String },
+    gender: { type: String },
+  },
+  { timestamps: true }
+);
+
+//Model - User class
+const User = mongoose.model("user", userSchema);
+
+// Middleware - Plugin
 app.use(express.urlencoded({ extended: false }));
 
 //Creating a Test middleware.
@@ -97,7 +118,7 @@ app
     }
   });
 
-app.post("/api/users", (req, res) => {
+app.post("/api/users", async (req, res) => {
   const body = req.body;
   if (
     !body.first_name ||
@@ -108,10 +129,20 @@ app.post("/api/users", (req, res) => {
   ) {
     return res.status(400).json({ error: "All fields are required" });
   }
-  users.push({ ...body, id: users.length + 1 });
-  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-    return res.status(201).json({ status: " Success ", id: users.length });
+  // Code to write in to a JSON file
+  // users.push({ ...body, id: users.length + 1 });
+  // fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+  //   return res.status(201).json({ status: " Success ", id: users.length });
+  // });
+  const result = await User.create({
+    firstName: body.first_name,
+    lastName: body.last_name,
+    email: body.email,
+    job_title: body.job_title,
+    gender: body.gender,
   });
+  // console.log(result);
+  return res.status(201).json({ status: "Success", id: result.id });
 });
 
 app.listen(PORT, () => console.log("Server is running on port " + PORT));
